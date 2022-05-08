cc4e1bfb8f1c2734339ea3317c92d5aee5efc356
jef0514 <joseph.frankland101@gmail.com>
2022-05-08 12:17:41 +0100

    Updated NN
    Updated NN that can get to 71% accuracy

--- ConvNetwork.py
+++ ConvNetwork.py
@@ -42,22 +42,30 @@ if __name__ == "__main__":
         info taken from here."""
         def __init__(self):
             super().__init__()
-            self.conv1 = nn.Conv2d(3, 6, 5) 
+            self.conv1 = nn.Conv2d(3, 16, 3) 
             self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(6, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 59)
-            self.fc4 = nn.Linear(59, 10)
+            self.conv2 = nn.Conv2d(16, 32, 3)
+            self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+            self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
+            self.conv5 = nn.Conv2d(128, 256, 3, padding=2)
+            self.fc1 = nn.Linear(512, 128)
+            self.fc2 = nn.Linear(128, 120)
+            self.fc3 = nn.Linear(120, 84)
+            self.fc4 = nn.Linear(84, 59)
+            self.fc5 = nn.Linear(59, 10)
 
         def forward(self, x):
             x = self.pool(F.relu(self.conv1(x)))
             x = self.pool(F.relu(self.conv2(x)))
+            x = self.pool(F.relu(self.conv3(x)))
+            x = self.pool(F.relu(self.conv4(x)))
+            x = self.pool(F.relu(self.conv5(x)))
             x = torch.flatten(x, 1) # flatten all dimensions except batch
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             x = F.relu(self.fc3(x))
-            x = self.fc4(x)
+            x = F.relu(self.fc4(x))
+            x = self.fc5(x)
             return x
 
 
@@ -68,7 +76,7 @@ if __name__ == "__main__":
     import torch.optim as optim
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001)
+    optimizer = optim.Adamax(net.parameters(), lr=0.001)
 
     start_time = time.time()
     seconds = 5
